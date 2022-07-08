44bff6cfcdeb0eabe3ad5e3ac83b5b11a02cd7ef
uhururawlings40 <uhururawlings40@gmail.com>
2022-07-08 13:48:14 +0300

    movie shop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,8 @@ export class HomeComponent implements OnInit {
   movies:any;
   display:any = "hide";
   ids:any;
+  search:any;
+  search_item:string = '';
   constructor(private PupolarService:PupolarService) { }
 
   ngOnInit(): void {
@@ -21,12 +23,12 @@ export class HomeComponent implements OnInit {
     let item_vars = localStorage.getItem("movies");
     if(item_vars){
       let data_obje = JSON.parse(item_vars)
-      this.movies = data_obje.results;
+      this.movies = data_obje;
       console.log(this.movies);
     }else{
       this.PupolarService.getMovies().subscribe((data:any) => {
         // this.movies = data;
-        localStorage.setItem("movies", JSON.stringify(data));
+        localStorage.setItem("movies", JSON.stringify(data.results));
       })
     }
   }
@@ -39,5 +41,29 @@ export class HomeComponent implements OnInit {
     }else{
       this.display = "hide"
     }
+  }
+  searchMovies(){
+    this.PupolarService.searchmovies(this.search_item).subscribe((data)=>{
+      this.search = data.results
+      console.log(this.search);
+      let movies:any = localStorage.getItem("movies");
+      // let movie_obj = JSON.parse(movies);
+      let movies_array:any = []
+      if(movies){
+      for(let i = 0; i < this.search.length; i++){
+        movies_array.push(this.search[i])
+      }
+      for(let j = 0; j < this.movies.length; j++){
+        movies_array.push(this.movies[j])
+      }
+      if(movies_array.length > 0){
+        localStorage.setItem("movies", JSON.stringify(movies_array));
+        console.log(movies_array.length);
+      }
+      }
+    })
+  }
+  scrollToTop(){
+    window.scroll({top : 0, behavior: "smooth"});
   }
 }
--- src/app/services/pupolar.service.ts
+++ src/app/services/pupolar.service.ts
@@ -7,10 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class PupolarService {
   api_key:string = "0922b153db69d52a188dfe55a4c24708"
+  url_searc:string = `https://api.themoviedb.org/3/search/company?api_key=${this.api_key}&page=`
   url:string = `https://api.themoviedb.org/3/movie/popular?api_key=${this.api_key}&language=en-US&page=120`
   constructor(private http:HttpClient) { }
 
   getMovies():Observable<any>{
     return this.http.get(this.url);
+  }
+
+  searchmovies(items:any):Observable<any>{
+    return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=0922b153db69d52a188dfe55a4c24708&language=en-US&query=${items}&page=1&include_adult=true`)
   }
 }
